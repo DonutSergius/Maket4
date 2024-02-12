95dbeaa88f7fe14b16e54eae75a3c8e73768163c
DonutSergius <popov.serg.2003.09@gmail.com>
2024-02-12 15:26:04 +0200

    Header menu

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,36 @@
 function getCurrentSection() {
-    const headerHeight = 150; // Розмір вашого header
-    const sections = document.querySelectorAll("section");
-    let currentSection = null;
+  const headerHeight = 150; // Розмір вашого header
+  const sections = document.querySelectorAll("section");
+  let currentSection = null;
 
-    sections.forEach((section) => {
-      const rect = section.getBoundingClientRect();
-      if (rect.top - headerHeight <= 50 && rect.bottom >= 50) {
-        currentSection = section.id;
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top - headerHeight <= 50 && rect.bottom >= 50) {
+      currentSection = section.id;
+    }
+  });
+
+  return currentSection;
+}
+
+function changeMenuBackground() {
+  const currentSection = getCurrentSection();
+  const headerButtons = document.querySelectorAll(".header-right button");
+  const menuButtons = document.querySelectorAll(".menu button");
+
+  if (window.innerWidth > 900) {
+    headerButtons.forEach((button) => {
+      const sectionId = button.getAttribute("data-section");
+
+      if (currentSection === sectionId) {
+        button.style.backgroundColor = "#68b7fd";
+        button.style.color = "#ffffff";
+      } else {
+        button.style.backgroundColor = "";
+        button.style.color = "";
       }
     });
-
-    return currentSection;
-  }
-
-  function changeMenuBackground() {
-    const currentSection = getCurrentSection();
-    const menuButtons = document.querySelectorAll(".header-right button");
-
+  } else {
     menuButtons.forEach((button) => {
       const sectionId = button.getAttribute("data-section");
 
@@ -24,21 +38,24 @@ function getCurrentSection() {
         button.style.backgroundColor = "#68b7fd";
         button.style.color = "#ffffff";
       } else {
-        button.style = ".header-right button";
+        button.style.backgroundColor = "";
+        button.style.color = "";
       }
     });
   }
+}
 
-  document.addEventListener("scroll", changeMenuBackground);
 
-  function scrollToSection(sectionId) {
-    const headerHeight = 150; // Розмір вашого header
-    const section = document.getElementById(sectionId);
+document.addEventListener("scroll", changeMenuBackground);
 
-    if (section) {
-      window.scrollTo({
-        top: section.offsetTop - headerHeight,
-        behavior: "smooth",
-      });
-    }
+function scrollToSection(sectionId) {
+  const headerHeight = 150; // Розмір вашого header
+  const section = document.getElementById(sectionId);
+
+  if (section) {
+    window.scrollTo({
+      top: section.offsetTop - headerHeight,
+      behavior: "smooth",
+    });
   }
+}
