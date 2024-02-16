5529506a4b23eb9ae7ded3f872fd4cfa68e8c05b
DonutSergius <popov.serg.2003.09@gmail.com>
2024-02-16 11:16:24 +0200

    Code optimization and Create comments

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
+// Зміна позиції користувача відповідно до натиснутої кнопки
 function getCurrentSection() {
-  const headerHeight = 200; // Розмір вашого header
+  const headerHeight = 200;
   const sections = document.querySelectorAll("section");
   let currentSection = null;
 
@@ -13,6 +14,8 @@ function getCurrentSection() {
   return currentSection;
 }
 
+
+// Зміна кольорів в залежності від позиції на сайті
 function changeMenuBackground() {
   const currentSection = getCurrentSection();
   const headerButtons = document.querySelectorAll(".header-right button");
@@ -20,6 +23,7 @@ function changeMenuBackground() {
 
   const transitionStyle = "background-color 0.3s ease, color 0.3s ease";
 
+  // для звичайного меню
   if (window.innerWidth > 900) {
     headerButtons.forEach((button) => {
       const sectionId = button.getAttribute("data-section");
@@ -32,7 +36,9 @@ function changeMenuBackground() {
         button.style.color = "";
       }
     });
-  } else {
+  } 
+  // для "гамбургер" меню
+  else {
     menuButtons.forEach((button) => {
       const sectionId = button.getAttribute("data-section");
       button.style.transition = transitionStyle;
@@ -51,7 +57,7 @@ function changeMenuBackground() {
 document.addEventListener("scroll", changeMenuBackground);
 
 function scrollToSection(sectionId) {
-  const headerHeight = 100; // Розмір вашого header
+  const headerHeight = 100;
   const section = document.getElementById(sectionId);
 
   if (section) {
